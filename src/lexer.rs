faee2f7c374ab8919b9cf2a2b2628b7374672b30
DavePearce <david.pearce@ecs.vuw.ac.nz>
2021-09-15 16:37:32 +1200

    Making progress on the scanner

--- src/lexer.rs
+++ src/lexer.rs
@@ -35,7 +35,12 @@ pub struct Lexer<'a> {
     offset: usize
 }
 
+/// An acceptor determines whether or not a character is part of a
+/// given token.
+type Acceptor = fn(char)->bool;
+
 impl<'a> Lexer<'a> {
+    /// Construct a new lexer for a given string slice.
     pub fn new(input: &'a str) -> Self {
         // Extract peekable iterator
         let chars = input.char_indices().peekable();
@@ -45,10 +50,64 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    /// Get the next token in the sequence, or none if we have reached
+    /// the end.
     pub fn next(&mut self) -> Option<Token> {
-        None
+        // Try and extract next character
+        let n = self.chars.next();
+        // Sanity check it
+        match n {
+            None => {
+                None
+            }
+            Some((offset,ch)) => {
+                self.scan(offset,ch)
+            }
+        }
+    }
+
+    /// Begin process of scanning a token based on its first
+    /// character.  The actual work is offloaded to a helper based on
+    /// this.
+    fn scan(&mut self, offset: usize, ch: char) -> Option<Token> {
+        if ch.is_digit(10) {
+            // Group all following digits together as an integer
+            self.scan_token(offset,TokenType::Integer,is_digit)
+        } else {
+            None
+        }
+    }
+
+    /// Gobble all characters matched by an acceptor into a token of a
+    /// given kind.  For example, we might want to continue matching
+    /// digits until we encounter something which isn't a digit (or is
+    /// the end of the file).
+    fn scan_token(&mut self, offset: usize, kind: TokenType, pred : Acceptor) -> Option<Token> {
+        // Continue reading whilst we're still matching characters
+        while let Some((o,c)) = self.chars.peek() {
+            if !pred(*c) {
+                // If we get here, then bumped into something which is
+                // not part of this token.
+                let content = &self.input[offset .. *o];
+                // Done
+                return Some(Token{kind: TokenType::Integer,start: offset,content})
+            }
+            // Move to next character
+            self.chars.next();
+        }
+        // If we get here, then ran out of characters.  So everything
+        // from the starting point onwards is part of the token.
+        let content = &self.input[offset .. ];
+        //
+        Some(Token{kind,start: offset,content})
     }
 }
+
+/// Determine whether a given character is a digit or not.
+fn is_digit(c: char) -> bool {
+    c.is_digit(10)
+}
+
 
 // ======================================================
 // Tests
@@ -57,6 +116,36 @@ impl<'a> Lexer<'a> {
 #[test]
 fn test_01() {
     let mut l = Lexer::new("");
+    assert!(l.next().is_none());
+}
+
+#[test]
+fn test_01b() {
+    let mut l = Lexer::new(" ");
+    assert!(l.next().is_none());
+}
+
+#[test]
+fn test_01c() {
+    let mut l = Lexer::new("  ");
+    assert!(l.next().is_none());
+}
+
+#[test]
+fn test_01d() {
+    let mut l = Lexer::new("\n");
+    assert!(l.next().is_none());
+}
+
+#[test]
+fn test_01e() {
+    let mut l = Lexer::new(" \n");
+    assert!(l.next().is_none());
+}
+
+#[test]
+fn test_01f() {
+    let mut l = Lexer::new("\n ");
     assert!(l.next().is_none());
 }
 
@@ -69,8 +158,32 @@ fn test_02() {
 }
 
 #[test]
+fn test_02b() {
+    let mut l = Lexer::new("  1");
+    assert!(l.next().unwrap().kind == TokenType::Integer);
+}
+
+#[test]
 fn test_03() {
     let mut l = Lexer::new("1234");
+    assert!(l.next().unwrap().kind == TokenType::Integer);
+}
+
+#[test]
+fn test_03b() {
+    let mut l = Lexer::new("1234 ");
+    assert!(l.next().unwrap().kind == TokenType::Integer);
+}
+
+#[test]
+fn test_0c() {
+    let mut l = Lexer::new("1234_");
+    assert!(l.next().unwrap().kind == TokenType::Integer);
+}
+
+#[test]
+fn test_0d() {
+    let mut l = Lexer::new("1234X");
     assert!(l.next().unwrap().kind == TokenType::Integer);
 }
 
@@ -79,6 +192,12 @@ fn test_03() {
 #[test]
 fn test_04() {
     let mut l = Lexer::new("abc");
+    assert!(l.next().unwrap().kind == TokenType::Identifier);
+}
+
+#[test]
+fn test_04b() {
+    let mut l = Lexer::new("  abc");
     assert!(l.next().unwrap().kind == TokenType::Identifier);
 }
 
